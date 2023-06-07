b493a6654fc47395adeeb22d1d92f20567f6aa98
Diego Ferreira <diegomecanica2014@gmail.com>
2023-06-07 18:56:53 -0400

    Fonte: importando fonte do google

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
     <title>Gerenciador de tarefas</title>
 </head>
 
@@ -18,8 +22,8 @@
         </div>
         <div class="form">
             <form action="" method="get">
-               <input type="text" name="task_name" placehholder="Nome da tarefa">
-               <button type="submit">Cadastrar</button>
+                <input type="text" name="task_name" placehholder="Nome da tarefa">
+                <button type="submit">Cadastrar</button>
             </form>
         </div>
         <div class="separator">
