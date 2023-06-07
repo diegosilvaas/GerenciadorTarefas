0012e0bc77af913c21ddcdaa184aa21398ee43da
Diego Ferreira <diegomecanica2014@gmail.com>
2023-06-07 18:55:36 -0400

    index: criando a listagem na tela

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerenciador de tarefas</title>
 </head>
+
 <body>
-    
+
+    <div class="container">
+        <div class="header">
+            <h1> Gerenciador de tarefas</h1>
+        </div>
+        <div class="class">
+        </div>
+        <div class="form">
+            <form action="" method="get">
+               <input type="text" name="task_name" placehholder="Nome da tarefa">
+               <button type="submit">Cadastrar</button>
+            </form>
+        </div>
+        <div class="separator">
+        </div>
+        <div class="list-tasks">
+            <ul>
+                <li>Tarefa 1</li>
+                <li>Tarefa 2</li>
+                <li>Tarefa 3</li>
+            </ul>
+        </div>
+        <div class="footer">
+            <p>Desenvolvido por Diego Ferreira</p>
+        </div>
+    </div>
+
 </body>
+
 </html>
